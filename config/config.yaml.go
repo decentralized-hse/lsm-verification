656816e5f35f44f3097c90bc2cd8782f184139dd
taiypeo <Qwertygid@gmail.com>
2023-03-31 16:59:23 +0000

    Update config

--- config/config.yaml.go
+++ config/config.yaml.go
@@ -1,10 +1,22 @@
 package config
+
 type Config struct {
 	RunEnv interface{} `yaml:"run_env"`
-	Env Env `yaml:"env"`
+	Env    Env         `yaml:"env"`
+	Db     Db          `yaml:"db"`
 }
 type Env struct {
+	EnvDb Db  `yaml:"db"`
+	Rsa   Rsa `yaml:"rsa"`
+}
+type EnvDb struct {
 	ServerAddress interface{} `yaml:"server_address"`
-	ReplicaID interface{} `yaml:"replica_id"`
+	ReplicaID     interface{} `yaml:"replica_id"`
+}
+type Rsa struct {
+	PublicKey  interface{} `yaml:"public_key"`
+	PrivateKey interface{} `yaml:"private_key"`
+}
+type Db struct {
 	BatchSize interface{} `yaml:"batch_size"`
 }
