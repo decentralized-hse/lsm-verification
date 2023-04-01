35c1455f29b671ffc42cda82a7a6f55dacc8516c
Panesher <panesher@yandex-team.ru>
2023-04-01 18:51:23 +0600

    console put added

---
+++ test_utils/populate.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"fmt"
+	"math/rand"
+
+	"lsm-verification/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func put(client proto.LSeqDatabaseClient, key, value string) error {
+	putRequest := &proto.PutRequest{
+		Key:   key,
+		Value: value,
+	}
+
+	log.Println("Requesting to append to the database")
+	_, err := client.Put(context.Background(), putRequest)
+	if err != nil {
+		return err
+	}
+	log.Println("Appended to the database")
+
+	return nil
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+func randSeq(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+
+func main() {
+	addr, exists := os.LookupEnv("dbServerAddress")
+	if !exists {
+		log.Fatalln("Env variable 'dbServerAddress' not found")
+	}
+
+	log.Println("Dialing GRPC")
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln("Failed to start grpc connection", err)
+	}
+
+	log.Println("Creating a database client")
+	client := proto.NewLSeqDatabaseClient(conn)
+
+	var mode string
+	fmt.Printf("Mode: ")
+	fmt.Scanf("%s\n", &mode)
+	if mode != "console" {
+		var count int
+		fmt.Printf("count: ")
+		fmt.Scanf("%d\n", &count)
+		
+		for i := 0; i < count; i+=1 {
+			key := randSeq(5)
+			value := randSeq(10)
+			put(client, key, value)
+			log.Println("Putted", key, value)	
+		}
+		return
+	}
+
+	for {
+		var key string
+		fmt.Printf("Key: ")
+		fmt.Scanf("%s\n", &key)
+	 
+		var value string
+		fmt.Print("Value: ")
+		fmt.Scanf("%s\n", &value)
+		fmt.Println(key, value)
+	}
+}
